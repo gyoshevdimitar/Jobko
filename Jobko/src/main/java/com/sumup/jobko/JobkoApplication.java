f99171702d024d4e5f15b9e6ed9911c34b912b17
dimitarsimeonov <dimitar.simeonov.a25@learn.telerikacademy.com>
2023-02-26 23:31:33 +0200

    Add project skeleton.

---
+++ Jobko/src/main/java/com/sumup/jobko/JobkoApplication.java
@@ -0,0 +1,13 @@
+package com.sumup.jobko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobkoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobkoApplication.class, args);
+    }
+
+}
---
+++ Jobko/src/test/java/com/sumup/jobko/JobkoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumup.jobko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobkoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
