6d2ad9a93d319aacaf358e28da78a06ba80ee9e5
dimitarsimeonov <dimitar.simeonov.a25@learn.telerikacademy.com>
2023-02-27 15:41:33 +0200

    Edit 'create.sql' script.

--- Jobko/db/create.sql
+++ Jobko/db/create.sql
@@ -1,13 +1,23 @@
-create database if not exists `jobko`;
+create database `jobko`;
 use `jobko`;
 
-create table if not exists tasks
+create table tasks
 (
-    id          int auto_increment
+    id      int auto_increment
         primary key,
-    name        varchar(16) not null,
-    command     varchar(66) not null,
-    requires_id int         null,
+    name    varchar(16) not null,
+    command varchar(66) not null,
     constraint name
         unique (name)
 );
+
+create table tasks_requirements
+(
+    task_id        int null,
+    requirement_id int null,
+    constraint requirements_fk
+        foreign key (requirement_id) references tasks (id),
+    constraint tasks_fk
+        foreign key (task_id) references tasks (id)
+);
+
